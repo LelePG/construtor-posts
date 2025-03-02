3b3d0e6546e7c62e48a1dd77c94d5d69ff975bfa
Letícia Garcez <lpgarcez@protonmail.com>
2025-03-02 20:18:34 -0300

    feature:adiciona a possibilidade de rezise do texto

--- src/components/imagem/TextoItem.tsx
+++ src/components/imagem/TextoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextoConfig } from "@/core/texto/TextoConfig";
 
 interface TextItemProps {
@@ -14,6 +14,13 @@ export default function TextoItem({
 	onMouseDown,
 	onClick,
 }: TextItemProps) {
+	const [resizeState, setResizeState] = useState({
+		largura: 200,
+		isResizing: false,
+		startX: 0,
+		startWidth: 200,
+	});
+
 	let alinhamento: React.CSSProperties["textAlign"] = "left";
 
 	if (config.justificacao === "centro") {
@@ -22,30 +29,67 @@ export default function TextoItem({
 		alinhamento = "right";
 	}
 
+	const handleMouseDownResize = (e: React.MouseEvent) => {
+		e.stopPropagation();
+		setResizeState((prevState) => ({
+			...prevState,
+			isResizing: true,
+			startX: e.clientX,
+			startWidth: prevState.largura,
+		}));
+	};
+
+	const handleMouseMove = (e: MouseEvent) => {
+		if (resizeState.isResizing) {
+			const newWidth =
+				resizeState.startWidth + (e.clientX - resizeState.startX);
+			setResizeState((prevState) => ({
+				...prevState,
+				largura: newWidth > 50 ? newWidth : 70,
+			}));
+		}
+	};
+
+	const handleMouseUp = () => {
+		setResizeState((prevState) => ({
+			...prevState,
+			isResizing: false,
+		}));
+	};
+
+	React.useEffect(() => {
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
+
+		return () => {
+			document.removeEventListener("mousemove", handleMouseMove);
+			document.removeEventListener("mouseup", handleMouseUp);
+		};
+	}, [resizeState.isResizing]);
+
 	return (
 		<div
-			className="absolute"
+			className="absolute border-2 border-red-500 leading-tight cursor-move"
 			style={{
 				left: `${posicao.x}px`,
 				top: `${posicao.y}px`,
-				cursor: "move",
 				textAlign: alinhamento,
+				width: `${resizeState.largura}px`,
+				color: config.cor,
+				fontWeight: config.negrito ? "bold" : "normal",
+				fontStyle: config.italico ? "italic" : "normal",
+				fontFamily: config.familiaFonte,
+				fontSize: `${config.tamanhoFonte}px`,
 			}}
 			onMouseDown={onMouseDown}
 			onClick={onClick}
 		>
-			<span
-				className="text-4xl leading-tight"
-				style={{
-					color: config.cor,
-					fontWeight: config.negrito ? "bold" : "normal",
-					fontStyle: config.italico ? "italic" : "normal",
-					fontSize: `${config.tamanhoFonte}px`,
-					fontFamily: config.familiaFonte,
-				}}
-			>
-				{config.texto}
-			</span>
+			{config.texto}
+
+			<div
+				onMouseDown={handleMouseDownResize}
+				className="w-1 h-full bg-blue-500 cursor-ew-resize absolute right-0 top-0"
+			/>
 		</div>
 	);
 }
